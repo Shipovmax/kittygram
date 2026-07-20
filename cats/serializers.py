d3167672c6d2e67a07aef8aa717694ce250044af
Shipov <mshipov.dev@gmail.com>
2026-07-21 00:26:37 +0300

    Apply black and isort formatting to all Python files

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,8 +1,9 @@
-from .models import Cat
 from rest_framework import serializers
+
+from .models import Cat
 
 
 class CatSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cat
-        fields = ('name', 'color', 'birth_year')
+        fields = ("name", "color", "birth_year")
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,13 +1,12 @@
-from rest_framework.routers import SimpleRouter
-
 from django.urls import include, path
+from rest_framework.routers import SimpleRouter
 
 from cats.views import CatViewSet
 
 # Create the router.
 router = SimpleRouter()
 # Register the viewset under the desired URL prefix.
-router.register('cats', CatViewSet)
+router.register("cats", CatViewSet)
 # Any number of "URL, viewset" pairs can be registered on the router,
 # e.g.:
 # router.register('owners', OwnerViewSet)
@@ -16,5 +15,5 @@ router.register('cats', CatViewSet)
 urlpatterns = [
     # All routes registered on the router are exposed via router.urls;
     # include them in the project's root urlconf.
-    path('', include(router.urls)),
+    path("", include(router.urls)),
 ]
